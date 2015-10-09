8677a951cbbda294739f23322904711ff4cbb48a
Mika Attila <radiantstatue@gmail.com>
2015-10-09 13:02:04 +0200

    Implement reading the submitted file

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+function onFileInputChange(e) {
+    var file = e.target.files[0];
+
+    if (!file) {
+        return;
+    }
+
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        var contents = e.target.result;
+        alert(contents);
+    };
+    reader.readAsText(file);
+}
+
 function start() {
-    alert("OK");
+    document.getElementById('file-input').addEventListener('change', onFileInputChange, false);
 }
